bf237e63217d2016309edf7469c0b155a3946d8c
iceghost <khangemail22@gmail.com>
2022-02-14 23:46:44 +0700

    Clean up

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { serve, sleep } from "./deps.ts";
 import { sendMessage } from "./messenger.ts";
 import * as moodle from "./moodle.ts";
 
-const FACEBOOK_TOKEN = Deno.env.get("FACEBOOK_TOKEN");
-if (!FACEBOOK_TOKEN) throw new Error("facebook token not found");
-
 const CATEGORIES = [
   "Học kỳ II năm học 2021-2022 (Semester 2 - Academic year 2021-2022)",
   "Học kỳ I năm học 2021-2022 (Semester 1 - Academic year 2021-2022)",
   "Hoạt động Sinh viên  (Student Activities)",
 ];
 
-const IntlDate = new Intl.DateTimeFormat("vi-VN", { dateStyle: 'full', timeStyle: 'long' });
+const IntlDate = new Intl.DateTimeFormat("vi-VN", {
+  dateStyle: "full",
+  timeStyle: "long",
+});
 const IntlList = new Intl.ListFormat("vi-VN");
 
 async function handler(_: Request): Promise<Response> {
@@ -34,9 +34,11 @@ async function handler(_: Request): Promise<Response> {
           IntlDate.format(new Date(detail.timeupdated * 1000))
         );
 
-        const message = `- ${module.name}\n\nModule: ${moodle.getModuleUrl(module)
-          }\n\nCourse: ${moodle.getCourseUrl(courseid)}\n\n${IntlList.format(timestamp)
-          }`;
+        const message = `- ${module.name}\n\nModule: ${
+          moodle.getModuleUrl(module)
+        }\n\nCourse: ${moodle.getCourseUrl(courseid)}\n\n${
+          IntlList.format(timestamp)
+        }`;
         await sendMessage(message);
       } else {
         await sendMessage(`- unknown instance, ${JSON.stringify(instance)}`);
